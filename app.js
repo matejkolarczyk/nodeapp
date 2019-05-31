bf6b4edd314c5cf0d0ed72af811547fc9e1f2bc0
matejkolarczyk <m.kolarczyk.st@spseiostrava.cz>
2019-05-31 08:46:53 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+/*function SayHello(name){
+    console.log('Hello '+name);
+}
+SayHello('Matej');
+console.log(window);
+console.log();
+
+setTimeout();
+clearTimeout();
+
+setInterval();
+clearInterval();
+
+window.console.log();
+var message = 'Ahoj';
+window.message = '';
+console.log(module);*/
+const logger = require('./logger');
+logger.log('message');
+
